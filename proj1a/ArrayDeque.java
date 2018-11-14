75482b7e4078d99bbf577970232db0ea7fb351ae
Whalesea-Gustav <asipirinofjim@gmail.com>
2018-11-15 01:00:37 +0800

    3rd submission proj1A ArrayDeque refactor

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,179 +1,226 @@
 public class ArrayDeque<T> {
-    private int size;
     private T[] items;
-    private int nextFirst;
-    private int nextLast;
+    private int size;
+    private int nextLast = 5;
+    private int nextFirst = 4;
 
     public ArrayDeque() {
         this.size = 0;
-        this.nextFirst = 7;
-        this.nextLast = 0;
-        this.items = (T []) new Object[8];
+        this.items = (T[]) new Object[8];
     }
-
     public void addFirst(T i) {
-        this.size++;
-        if (this.nextFirst == this.nextLast) {
-            this.resize(2 * this.items.length);
+        /*this.nextFirst == this.nextLast*/
+        /*this.size == this.items.length - 1*/
+        if (this.size == this.items.length - 1) {
+           this.resize(this.items.length * 2);
         }
+        this.size++;
         this.items[this.nextFirst] = i;
-        this.nextFirst--;
+        this.nextFirstMinusOne(); // if exceed, flag == 0;
     }
-
-    public void addLast(T i) {
-        this.size++;
-        if (this.nextLast == this.nextFirst) {
-            this.resize(2 * this.items.length);
+    private void nextFirstMinusOne() {
+        if (this.nextFirst == 0) {
+            this.nextFirst = this.items.length - 1;
+        } else {
+            this.nextFirst--;
         }
-        this.items[this.nextLast] = i;
-        this.nextLast++;
     }
-
+    public void addLast(T i) {
+        /*this.next == this.first*/
+        /*this.size == this.items.length - 1*/
+        if (this.size == this.items.length - 1) {
+            this.resize(this.items.length * 2);
+        }
+        this.size++;
+        this.items[nextLast] = i;
+        this.nextLastAddOne();
+    }
+    private void nextLastAddOne() {
+        if (this.nextLast == this.items.length - 1) {
+            this.nextLast = 0;
+        } else {
+            this.nextLast++;
+        }
+    }
+    public void resize(int capacity) {
+        T[] temp = (T[]) new Object[capacity];
+        //calculate the difference between capacity and items length
+        // in case of capacity is less than items length
+        int dif = capacity - this.items.length;
+        System.arraycopy(this.items, 0,
+                               temp, 0, this.nextLast);
+        this.nextFirst += dif;
+        if (this.nextFirst == capacity - 1) {
+            return;
+        } else {
+            System.arraycopy(this.items, this.nextFirst - dif + 1,
+                             temp, this.nextFirst + 1, capacity - 1 - this.nextFirst);
+        }
+        this.items = temp;
+    }
     public boolean isEmpty() {
-        if ((this.nextFirst == this.items.length - 1) && this.nextLast == 0) {
+        if (this.size == 0) {
             return true;
         } else {
             return false;
         }
     }
-
-    public void printDeque() {
-
-        for (int start = this.nextFirst + 1; start < this.items.length; start++) {
-            System.out.print(this.items[start] + " ");
-        }
-
-        for (int start = 0; start < this.nextLast; start++) {
-            System.out.print(this.items[start] + " ");
-        }
-        System.out.print("\n");
-    }
-
-    private void resize(int capacity) {
-        T[] temp = (T[]) new Object[capacity];
-        //this.size = capacity;
-        int dif = capacity - this.items.length;
-        //  +++++++ nextLast/nextFirst + +  +  +  +  +
-        //  0123456         7          8 9 10 11 12 13
-        System.arraycopy(this.items, 0, temp, 0, nextFirst);
-        System.arraycopy(this.items, this.nextLast + 1, temp,
-                nextLast + 1 + dif, this.items.length - 1 - this.nextFirst);
-        this.items = temp;
-        this.nextFirst += dif;
-
-    }
-
-    public T removeLast() {
-        if (this.nextLast == 0) {
-            return null;
-        }
-        this.size--;
-        if (this.size * 4 <= this.items.length) {
-            this.resize(this.size * 2);
-        }
-        this.nextLast--;
-        T result = this.items[this.nextLast];
-        this.items[this.nextLast] = null;
-        return result;
-    }
-
-    public T removeFirst() {
-        if (this.nextFirst == this.items.length - 1) {
-            return null;
-        }
-
-        this.size--;
-        if (this.size * 4 <= this.items.length) {
-            this.resize(this.size * 2);
-        }
-        this.nextFirst++;
-        T result = this.items[this.nextFirst];
-        this.items[this.nextFirst] = null;
-        return result;
-    }
-
     public T get(int index) {
-        if (index > this.size - 1) {
-            return null;
-        }
-
-        if (index <= (this.items.length - 1 - this.nextFirst - 1)) {
+        if (this.nextFirst < this.nextLast) {
             return this.items[this.nextFirst + index + 1];
+        } else if (this.nextFirst == this.items.length-1) {
+            return this.items[index];
+        } else if (index <= (this.items.length - this.nextFirst - 2)) {
+            return this.items[index + this.nextFirst + 1];
         } else {
-            return this.items[index - this.items.length + 1 + this.nextFirst];
+            return this.items[index -
+                              (this.items.length - this.nextFirst - 1)];
         }
     }
-
-    public int size() {
-        return this.size;
+    public void printDeque() {
+        if (this.isEmpty()) {
+            System.out.println("");
+        } else if (this.nextFirst < this.nextLast) {
+            for (int i = this.nextFirst + 1; i < nextLast; i++) {
+                System.out.print(this.items[i] + " ");
+            }
+            System.out.print("\n");
+        } else if (this.nextFirst == this.items.length - 1) {
+            for (int i = 0; i < this.nextLast; i++) {
+                System.out.print(this.items[i]);
+            }
+            System.out.print("\n");
+        } else {
+            for (int i = this.nextFirst + 1; i <= this.items.length - 1; i++) {
+                System.out.print(this.items[i] + " ");
+            }
+            for (int i = 0; i < this.nextLast; i++) {
+                System.out.print(this.items[i] + " ");
+            }
+            System.out.print("\n");
+        }
     }
+    public T removeLast() {
+        T result;
+        if (this.isEmpty()) {
+            result = null;
+        } else {
+            this.nextLastMinusOne();
+            this.size--;
+            result = this.items[this.nextLast];
+            this.items[this.nextLast] = null;
+        }
 
-    private void nextLastMinusOneAux() {
-        if (this.nextLast >0) {
+        if ((this.size * 4 <= this.items.length) && (this.items.length >= 16)) {
+            this.resize(this.size * 2);
+        }
+
+        return result;
+    }
+    public void nextLastMinusOne() {
+        if (this.nextLast == 0) {
+            this.nextLast = this.items.length;
+        } else {
             this.nextLast--;
-        } else {
-            return;
         }
     }
-
-    private void nextFirstMinusOneAux() {
-        if (this.nextFirst == this.items.length) {
+    public T removeFirst() {
+        T result;
+        if (this.isEmpty()) {
+            result = null;
+        } else {
+            this.nextFirstAddOne();
+            result = this.items[this.nextFirst];
+            this.size--;
+        }
+        if(this.size * 4 <= this.items.length) {
+            this.resize(this.size * 2);
+        }
+        return result;
+    }
+    public void nextFirstAddOne() {
+        if (this.nextFirst == this.items.length - 1) {
+            this.nextFirst = 0;
+        } else {
             this.nextFirst++;
-        } else {
-            return;
         }
     }
 
-/*
-    public static void main(String [] args) {
+  /*  public static void main(String[] args) {
+        *//*
         ArrayDeque<Integer> deque1 = new ArrayDeque<>();
-        System.out.println("-----remove From Empty Deque");
-        System.out.println(deque1.removeFirst());
-
-        System.out.println("-----add 50 40 30 20 10");
-        deque1.addFirst(10);
-        deque1.addFirst(20);
-        deque1.addFirst(30);
-        deque1.addFirst(40);
-        deque1.addFirst(50);
-        System.out.println("-----get(0)-----");
+        deque1.addFirst(4);
+        deque1.addFirst(3);
+        deque1.addFirst(2);
+        deque1.addFirst(1);
+        deque1.addFirst(0);
+        deque1.addFirst(5);
+        deque1.addFirst(6);
+        deque1.addFirst(7);
+        deque1.addFirst(10 );
+        System.out.println("------get()------");
         System.out.println(deque1.get(0));
-
-        System.out.println("-----get(n)-----");
         System.out.println(deque1.get(1));
         System.out.println(deque1.get(2));
         System.out.println(deque1.get(3));
         System.out.println(deque1.get(4));
         System.out.println(deque1.get(5));
-        deque1.printDeque();
-
-
-        System.out.println("-----get(x)-----");
-        deque1.addLast(0);
-        System.out.println(deque1.get(5));
-        deque1.addLast(-10);
         System.out.println(deque1.get(6));
         System.out.println(deque1.get(7));
-        System.out.println(deque1.size());
+        System.out.println(deque1.get(8));
+        System.out.println(deque1.get(9));
+        *//*
+        *//*
+        ArrayDeque<Integer> deque2 = new ArrayDeque<>();
+        deque2.addLast(0);
+        deque2.addLast(1);
+        deque2.addLast(2);
+        deque2.addLast(3);
+        deque2.addLast(4);
+        deque2.addFirst(5);
+        deque2.addFirst(6);
+        deque2.addFirst(7);
+        deque2.addFirst(8);
+        deque2.addFirst(9);
+        System.out.println(deque2.get(0));
+        System.out.println(deque2.get(1));
+        System.out.println(deque2.get(2));
+        System.out.println(deque2.get(3));
+        System.out.println(deque2.get(4));
+        System.out.println(deque2.get(5));
+        System.out.println(deque2.get(6));
+        System.out.println(deque2.get(7));
+        System.out.println(deque2.get(8));
+        System.out.println(deque2.get(9));
+        System.out.println(deque2.get(10));
+        *//*
+        *//*
+        ArrayDeque<Integer> deque3 = new ArrayDeque<>();
+        System.out.println(deque3.isEmpty());
+        deque3.addLast(10);
+        System.out.println(deque3.isEmpty());
+        *//*
 
-        System.out.println("-----exceed and resize-----");
-        deque1.addFirst(60);
-        System.out.println(deque1.get(0));
-        deque1.addLast(-20);
-        deque1.addLast(-30);
-
-        deque1.printDeque();
-
-        System.out.println("-----removeLast()-----");
-        deque1.removeLast();
-        deque1.printDeque();
-
-        System.out.println("-----removeFirst()-----");
-        deque1.removeFirst();
-        deque1.printDeque();
-        System.out.println(deque1.get(7));
+        *//*
+        ArrayDeque<Integer> deque4 = new ArrayDeque<>();
+        deque4.addLast(0);
+        deque4.addLast(1);
+        deque4.addLast(2);
+        deque4.addLast(3);
+        deque4.addLast(4);
+        deque4.addFirst(5);
+        deque4.addFirst(6);
+        deque4.addFirst(7);
+        deque4.addFirst(8);
+        deque4.printDeque();
+        *//*
 
 
-   }
-*/
+        ArrayDeque<Integer> deque5 = new ArrayDeque<>();
+        deque5.addFirst(10);
+        deque5.printDeque();
+        deque5.removeLast();
+        System.out.println(deque5.isEmpty());
+    }*/
+
 }
