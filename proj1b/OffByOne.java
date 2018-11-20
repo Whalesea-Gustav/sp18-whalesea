8e1e74de66de38d34f6a2c45fc6411b51c234643
Whalesea-Gustav <asipirinofjim@gmail.com>
2018-11-20 16:35:09 +0800

    2rd submission proj1b modification

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,7 +1,11 @@
 public class OffByOne implements CharacterComparator{
     @Override
     public boolean equalChars(char x, char y) {
-        return (Math.abs(x - y) == 1);
+        if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')) {
+            return (Math.abs(x - y) == 1);
+        } else {
+            return false;
+        }
     }
 
 }
